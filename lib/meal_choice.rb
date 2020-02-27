c92d1344981f5bd488c900a25415e1ae005810b8
maxwellbenton <maxwellbenton@gmail.com>
2020-02-27 16:55:57 -0500

    update solution

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,9 +4,9 @@
 # "A plate of #{protein} with #{veg1} and #{veg2}"
 # and `puts` "What a nutritious meal!"
 
-def meal_choice(veg1, veg2, protein='meat')
+def meal_choice(veg1, veg2, protein='tofu')
   puts "What a nutritious meal!"
   meal = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  return meal # return here is optional. Ruby methods automatically return the last expression
+  meal
 end
